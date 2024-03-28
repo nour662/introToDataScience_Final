da4154212cdeeb2d4f2735f690777af54e9ca2ed
nour662 <nourali1786@gmail.com>
2024-03-28 17:10:27 -0400

    adding folder and dataset for milestone 2

--- dag.py
+++
@@ -1,8 +0,0 @@
-import airflow 
-from airflow import PythonOperator 
-from datetime import timedelta, datetime
-
-default_args = {
-        'owner' : 'airflow',
-        'start_date' : datetime(2024, 1,1 )
-}2
