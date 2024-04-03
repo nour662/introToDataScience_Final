4ade8f35c7ea70ba77ed3fcc2bc67bc7c1b6c4e8
Kurubel Belay <kuru.belay@wireless-10-104-73-186.umd.edu>
2024-04-03 18:52:50 -0400

    adding Question 2

--- Milestone_2/milestone_2_code.R
+++ Milestone_2/milestone_2_code.R
@@ -1,7 +1,89 @@
+##Load Libraries##
 library(ggplot2)
 library(gridExtra)
 library(rpart)
 library(png)
+
+library(caTools)
+library(car)
+library(glmnet)
+library(gmodels)
+library(e1071)
+
+### Question #2: Log Regression and Naive Bayes ###
+
+#Import dataset and create a subset that removes NA cases
+dataset <- read.csv("cleaned_datset.csv")
+complete_data <- complete.cases(dataset)
+data_subset <- dataset[complete_data,]
+
+#Create a clean data frame with the main variables and standardize
+clean_subset <- data_subset[,c("t_cap", "t_hh","t_rd","t_rsa","t_ttlh")]
+standard <- scale(clean_subset)
+
+#Regularization Code
+#Create training and testing sets
+testidx <- which(1:nrow(standard)%%4==0)
+data_train <- standard[-testidx,]
+data_test <- standard[testidx,]
+
+#Run ridge regression, plot and check coefficients
+cv.fit <- cv.glmnet(as.matrix(data_train[,c(-1)]),as.vector(data_train[,1]),alpha=0)
+plot(cv.fit)
+coef(cv.fit)
+
+##Check for minimum lambda value
+cv.fit$lambda.min
+
+#Compare correlation between regularized model and actual testing set
+prediction <- predict(cv.fit,newx = as.matrix(data_test[,c(-1)]))
+cor(prediction,as.vector(data_test[,1]))
+
+#Log Regression Code
+#Convert the dependent variable into a categorical → above >1700 is a high performing 1, <1700 is not 0
+clean_subset$t_cap_bin <- ifelse(clean_subset$t_cap > 1700, 1,0)
+
+#Randomize and create training (75%) and testing (25%) sets
+set.seed(123)
+split <- sample.split(clean_subset$t_cap_bin, SplitRatio <- 0.75)
+training_set <- subset(clean_subset, split ==TRUE)
+test_set <- subset(clean_subset, split ==FALSE)
+
+# Build log regression model excluding the original column with numerical values for the dependent variable
+classifier <- glm(t_cap_bin~.-t_cap, family = binomial,data =training_set)
+
+#Check statistical significance
+summary(classifier)
+
+#Exponentiate coefficients to interpret as odd-ratios
+exp(coef(classifier))
+
+#Use the predict function to generate predicted probabilities of the dependent variable using the log model
+prob_pred <- predict(classifier,type="response",newdata = test_set)
+
+#Convert probabilities to 1 and 0 if larger than 0.5 probability 
+y_pred <- ifelse(prob_pred > 0.5, 1,0)
+
+#Use a confusion matrix to evaluate quality of predicted probabilities
+CrossTable(test_set[,6], y_pred,prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE, dnn = c('predicted','actual'))
+
+#Naive Bayes Code
+# Convert t_cap_bin to a factor with levels "No" and "Yes"
+clean_subset$t_cap_bin <- factor(clean_subset$t_cap_bin, levels = c(0, 1), labels = c("No", "Yes"))
+
+# Convert across previously defined training and testing sets from log regression
+training_set$t_cap_bin <- factor(training_set$t_cap_bin, levels = c(0, 1), labels = c("No", "Yes"))
+test_set$t_cap_bin <- factor(test_set$t_cap_bin, levels = c(0, 1), labels = c("No", "Yes"))
+
+# Train the NB Classifier, remove first column of original t_cap values
+nb_classifer <- naiveBayes(training_set[,-1],training_set$t_cap_bin)
+
+#Predict on testing set
+nb_predict <- predict(nb_classifier, newdata = testing_set)
+
+# evaluate using Confusion Matrix
+CrossTable(nb_predict, test_set$t_cap_bin,prop.chisq = FALSE, prop.t = FALSE, prop.r = FALSE, dnn = c('predicted', 'actual'))
+
 ### Question 3: Decision Trees and Random Forests ############
 
 # Reading in dataset
